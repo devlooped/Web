3ec003957ca22238034270b397bfac9eedc676a8
Daniel Cazzulino <daniel@cazzulino.com>
2022-07-13 03:04:23 -0300

    Allow tuning whitespace handling and case folding while reading
    Fixes #1

--- src/Html/HtmlDocument.cs
+++ src/Html/HtmlDocument.cs
@@ -56,6 +56,52 @@ public static class HtmlDocument
     public static XDocument Load(Stream stream)
         => Load(new StreamReader(stream));
 
+    /// <overloads>
+    /// The Load method provides multiple strategies for creating a new
+    /// <see cref="XDocument"/> and initializing it from a data source containing
+    /// raw XML.  Load from a file (passing in a URI to the file), a
+    /// <see cref="Stream"/> or an a <see cref="TextReader"/>. 
+    /// Note:  Use <see cref="Parse(string)"/>
+    /// to create an <see cref="XDocument"/> from a string containing HTML.
+    /// <seealso cref="Parse(string)"/>
+    /// </overloads>
+    /// <summary>
+    /// Create a new <see cref="XDocument"/> based on the contents of the file
+    /// referenced by the URI parameter passed in. Note: Use
+    /// <see cref="Parse(string)"/> to create an <see cref="XDocument"/> from
+    /// a string containing HTML.
+    /// <seealso cref="Parse(string)"/>
+    /// </summary>
+    /// <param name="uri">
+    /// A URI string referencing the file to load into a new <see cref="XDocument"/>.
+    /// </param>
+    /// <param name="settings">The settings for the HTML load process.</param>
+    /// <returns>
+    /// An <see cref="XDocument"/> initialized with the contents of the file referenced
+    /// in the passed in uri parameter.
+    /// </returns>
+    public static XDocument Load(string uri, HtmlReaderSettings settings)
+    {
+        using var stream = (Stream)new XmlUrlResolver().GetEntity(new Uri(uri), string.Empty, typeof(Stream));
+        return Load(stream, settings);
+    }
+
+    /// <summary>
+    /// Create a new <see cref="XDocument"/> and initialize its underlying XML tree using
+    /// the passed <see cref="Stream"/> parameter.
+    /// </summary>
+    /// <param name="stream">
+    /// A <see cref="Stream"/> containing the raw HTML to read into the newly
+    /// created <see cref="XDocument"/>.
+    /// </param>
+    /// <param name="settings">The settings for the HTML load process.</param>
+    /// <returns>
+    /// A new <see cref="XDocument"/> containing the contents of the passed in
+    /// <see cref="Stream"/>.
+    /// </returns>
+    public static XDocument Load(Stream stream, HtmlReaderSettings settings)
+        => Load(new StreamReader(stream), settings);
+
     /// <summary>
     /// Create a new <see cref="XDocument"/> and initialize its underlying XML tree using
     /// the passed <see cref="TextReader"/> parameter.  Optionally whitespace handling
@@ -71,16 +117,36 @@ public static class HtmlDocument
     /// </returns>
     public static XDocument Load(TextReader textReader)
     {
-        using var reader = new SgmlReader(new XmlReaderSettings
-        {
-            CheckCharacters = true,
-            IgnoreComments = true,
-            IgnoreProcessingInstructions = true,
-            IgnoreWhitespace = true,
-        })
+        using var reader = new SgmlReader
         {
             InputStream = textReader,
-            WhitespaceHandling = WhitespaceHandling.Significant,
+        };
+
+        return XDocument.Load(new XhtmlContentReader(reader));
+    }
+
+    /// <summary>
+    /// Create a new <see cref="XDocument"/> and initialize its underlying XML tree using
+    /// the passed <see cref="TextReader"/> parameter.  Optionally whitespace handling
+    /// can be preserved.
+    /// </summary>
+    /// <param name="textReader">
+    /// A <see cref="TextReader"/> containing the raw HTML to read into the newly
+    /// created <see cref="XDocument"/>.
+    /// </param>
+    /// <param name="settings">The settings for the HTML load process.</param>
+    /// <returns>
+    /// A new <see cref="XDocument"/> containing the contents of the passed in
+    /// <see cref="TextReader"/>.
+    /// </returns>
+    public static XDocument Load(TextReader textReader, HtmlReaderSettings settings)
+    {
+        using var reader = new SgmlReader
+        {
+            InputStream = textReader,
+            CaseFolding = settings.CaseFolding,
+            WhitespaceHandling = settings.WhitespaceHandling,
+            TextWhitespace = settings.TextWhitespace,
         };
 
         return XDocument.Load(new XhtmlContentReader(reader));
@@ -94,15 +160,32 @@ public static class HtmlDocument
     /// Create a new <see cref="XDocument"/> from a string containing
     /// HTML.
     /// </summary>
-    /// <param name="html">
-    /// A string containing HTML.
-    /// </param>
+    /// <param name="html">A string containing HTML.</param>
     /// <returns>
     /// An <see cref="XDocument"/> containing an XML tree initialized from the
     /// passed in HTML string.
     /// </returns>
     public static XDocument Parse(string html)
         => Load(new StringReader(html));
+
+    /// <overloads>
+    /// Create a new <see cref="XDocument"/> from a string containing
+    /// HMTL.
+    /// </overloads>
+    /// <summary>
+    /// Create a new <see cref="XDocument"/> from a string containing
+    /// HTML.
+    /// </summary>
+    /// <param name="html">
+    /// A string containing HTML.
+    /// </param>
+    /// <param name="settings">The settings for the HTML load process.</param>
+    /// <returns>
+    /// An <see cref="XDocument"/> containing an XML tree initialized from the
+    /// passed in HTML string.
+    /// </returns>
+    public static XDocument Parse(string html, HtmlReaderSettings settings)
+        => Load(new StringReader(html), settings);
 
     /// <summary>
     /// Skips known non-content elements such as scripts and styles. 
---
+++ src/Html/HtmlReaderSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+using Sgml;
+
+namespace Devlooped.Html;
+
+/// <summary>
+/// Specifies a set of features to support when loading 
+/// HTML via <see cref="HtmlDocument"/>.
+/// </summary>
+public sealed class HtmlReaderSettings
+{
+    /// <summary>
+    /// HTML is case insensitive, so you can choose between converting
+    /// to lower case or upper case tags. "None" means that the case is left
+    /// alone, except that end tags will be folded to match the start tags.
+    /// </summary>
+    public CaseFolding CaseFolding { get; set; }
+
+    /// <summary>
+    /// Specifies how leading and trailing whitespace is handled.
+    /// Note that this is a <see cref="FlagsAttribute"/>-enum.
+    /// </summary>
+    public TextWhitespaceHandling TextWhitespace { get; set; }
+
+    /// <summary>
+    /// Specifies how whitespace nodes are handled.
+    /// </summary>
+    public WhitespaceHandling WhitespaceHandling { get; set; }
+}
